bb0a093f8072222fdc8543d73bb0443143c919a9
kemoeverlyne <kemoeverlyne@gmail.com>
2021-05-22 14:51:09 +0300

    third party api

--- stockex/settings.py
+++ stockex/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'market.apps.MarketConfig'
+    'market.apps.MarketConfig',
 ]
 
 MIDDLEWARE = [
